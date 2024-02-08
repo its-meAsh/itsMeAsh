2e9c7f95a6ad26471fb3594861a16706e189dbad
Ash <136570879+its-meAsh@users.noreply.github.com>
2024-02-08 20:37:34 +0530

    Update docsGoTo.js

--- docsGoTo.js
+++ docsGoTo.js
@@ -3,3 +3,6 @@ guideOfProjects.addEventListener('click', () => { location.replace("docs/guideOf
 
 const tns = document.querySelector("#tns");
 tns.addEventListener('click', () => { location.replace("docs/tns/tns.html") });
+
+const logicCircuit = document.querySelector("#logicCircuit");
+logicCircuit.addEventListener('click', () => { location.replace("docs/logicCircuit/logicCircuit.html") });
